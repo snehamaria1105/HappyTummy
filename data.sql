77e73a22f9f6b4934a6303ae7799df78378365d9
snehamaria1105 <snehamariadev@gmail.com>
2026-02-25 09:16:32 +0530

    added dummy data to Restaurant table

---
+++ data.sql
@@ -0,0 +1,11 @@
+INSERT INTO RESTAURANT (restaurantName, phone, location, imageurl) VALUES
+('Pizza Palace', '123-456-7890', 'Vellayambalom', 'https://example.com/pizza_palace.jpg'),
+('Sushi Kada', '987-654-3210', 'Kowdiar', 'https://example.com/sushi_kada.jpg'),
+('Njetikkal Mandhi Kada', '555-555-5555', 'Poojapura', 'https://example.com/njetikkal_mandhi_kada.jpg'),
+('Howweda Chayakada', '555-555-5555', 'Palayam', 'https://example.com/njetikkal_mandhi_kada.jpg'),
+('Ikkakkante Sarbath', '555-555-5555', 'Pattom', 'https://example.com/njetikkal_mandhi_kada.jpg'),
+('Matcha Kudicha?', '555-555-5555', 'Kuravankonam', 'https://example.com/njetikkal_mandhi_kada.jpg'),
+('Vedikkett Burger', '555-555-5555', 'Peyad', 'https://example.com/njetikkal_mandhi_kada.jpg'),
+('Porotta Adich keri Vaa', '555-555-5555', 'Pattom', 'https://example.com/njetikkal_mandhi_kada.jpg');
+
+
